7f33371b2a628fb10967c8f9e0b177a7bc03a233
Gaurav Heda <hedagaurav93@gmail.com>
2024-04-27 16:31:59 +0530

    first commit with project and task model,migration, controller, factory, seeder.

---
+++ resources/views/guide.blade.php
@@ -0,0 +1,12 @@
+Tables – 
+Project (Project Code, Project Name)
+Task (Task Name, Task Hours)
+Details – 
+1.	Create Tables as mentioned above
+2.	All the data is required, and Project Code cannot be duplicate
+3.	Create a screen with Add/Edit form to capture Project and Tasks details
+4.	There should be option to add multiple Tasks for each project and can Edit/Delete
+5.	Once the form is submitted, it should navigate to the list screen
+6.	There should be an option to filter the project based on name and code
+
+
